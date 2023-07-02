95cb0838fe92f133593999fe9469f052849dde7e
lew-monk <lewmonk@gmail.com>
2023-07-02 17:30:00 +0300

    Add Dapi Api

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import DapiApp from "@dapi-co/dapi-node";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 
@@ -5,9 +6,28 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const dapiSecret = process.env.DAPI_SECRET_KEY;
+
+console.log(dapiSecret);
+const dapi = new DapiApp({
+  appSecret: dapiSecret!,
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+app.post("/dapi", async (req, res) => {
+  try {
+    const dapiResponse = await dapi.handleSDKDapiRequests(
+      req.body,
+      req.headers
+    );
+    res.send(dapiResponse);
+  } catch (error) {
+    //Handle Network Errors
+    console.dir(error);
+  }
 });
 
 app.listen(port, () => {
