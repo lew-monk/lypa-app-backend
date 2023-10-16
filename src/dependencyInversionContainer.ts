d238690ff5d4c4569c0717ee81d80bd49b976cf3
lew-monk <lewmonk@gmail.com>
2023-10-16 18:45:02 +0300

    Configure Inversify container

--- src/dependencyInversionContainer.ts
+++ src/dependencyInversionContainer.ts
@@ -1,7 +1,9 @@
 import { Container } from "inversify";
 import { DBServiceImpl } from "./data/db";
 import { MpesaRepository } from "./data/mpesa/mpesa.repository";
+import { UserRepository } from "./data/users/user.repository";
 import { MpesaService } from "./logic/mpesa/mpesa";
+import { UserService } from "./logic/users/users";
 
 export const container = new Container({
   defaultScope: "Singleton",
@@ -10,3 +12,5 @@ export const container = new Container({
 container.bind(DBServiceImpl).toSelf();
 container.bind(MpesaService).toSelf();
 container.bind(MpesaRepository).toSelf();
+container.bind(UserService).toSelf();
+container.bind(UserRepository).toSelf();
