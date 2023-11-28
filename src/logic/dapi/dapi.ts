72001675621dc7ec881b1b5d3a9b32a86a73a2dd
lew-monk <lewmonk@gmail.com>
2023-11-28 22:40:44 +0300

    Add Banking Support

--- src/logic/dapi/dapi.ts
+++ src/logic/dapi/dapi.ts
@@ -8,16 +8,24 @@ const dapi = new DapiApp({
 
 @injectable()
 export class DapiService {
-  public constructor(
-    private readonly _accessToken: string = process.env.DAPI_TOKEN!,
-    private readonly _userSecret: string = process.env.DAPI_USER_SECRET!
-  ) {}
+  // public constructor(
+  //   private readonly _accessToken: string = process.env.DAPI_TOKEN!,
+  //   private readonly _userSecret: string = process.env.DAPI_USER_SECRET!
+  // ) {}
 
-  public async getIdentity() {
-    const { status, identity, msg } = await dapi.data.getIdentity(this._accessToken, this._userSecret);
-    if (status === "done") {
-      return identity;
-    } else if (status === "failed") {
+  // public async getIdentity() {
+  //   const { status, identity, msg } = await dapi.data.getIdentity();
+  //   if (status === "done") {
+  //     return identity;
+  //   } else if (status === "failed") {
+  //     throw new ValidationException(msg!);
+  //   } else throw new ValidationException("Pending");
+  // }
+
+  public async exchangeToken(accessCode: string, connectionID: string): Promise<string> {
+    const { accessToken, status, msg } = await dapi.auth.exchangeToken(accessCode, connectionID);
+    if (accessToken) return accessToken;
+    else if (status === "failed") {
       throw new ValidationException(msg!);
     } else throw new ValidationException("Pending");
   }
